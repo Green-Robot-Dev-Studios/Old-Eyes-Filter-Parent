2bad71763d25889d1dcfd003a52ed67654ecf24e
Nicholas Ficara <nick@nicholasficara.dev>
2023-07-11 23:07:22 -0400

    Added content and reorganized files

---
+++ js/index.js
@@ -0,0 +1,9 @@
+VANTA.CLOUDS({
+    el: "#clouds",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00
+})
+twemoji.parse(document.body);
